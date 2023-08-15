e2716a4b1a35b98992c41cc4325ac3580a6265d1
Luiz Almada <luizalmada@yahoo.com>
2023-08-15 11:21:06 -0300

    Implementa screen tests

--- __tests__/CoinsList.test.tsx
+++ __tests__/CoinsList.test.tsx
@@ -1,9 +1,54 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import CoinsList from "@/pages/components/CoinsList";
 
-describe("General CoinsList test", () => {
-    test("It should render", () => {
-        render(<CoinsList />)
-    });
-});
+type FetchMock = jest.Mock<Promise<Response>>;
+type FetchData = Record<string, any>;
 
+const setFetchReturnData = (data: FetchData): FetchMock => {
+  // const fetchMock: FetchMock = jest.fn(() =>
+  const fetchMock: any = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(data),
+    })
+  );
+
+  global.fetch = fetchMock;
+
+  return fetchMock;
+};
+describe("General CoinsList test", () => {
+  beforeAll(() => {
+    setFetchReturnData([{id: "bitcoin"}, {id: "ethereum"}]);
+  });
+  
+  test("It should render", () => {
+      render(<CoinsList />)
+  });
+
+  it("Should render API data", async () => {
+    render(<CoinsList />)
+
+    await screen.findByText("bitcoin");
+  });
+
+  test("It should filter correctly", async () => {
+    render(<CoinsList />);
+    
+    await screen.findByText("bitcoin");
+    await screen.findByText("ethereum");
+
+    const filter = screen.getByLabelText(/filter/i);
+    fireEvent.change(filter, {target : { value : "bitcoin"}});
+    
+    // await screen.getByText("bitcoin");
+
+    // Como o fireEvent já aguarda o processamento
+    // então pode dispensar o await
+    screen.getByText("bitcoin");
+    
+    expect(screen.queryByText("ethereum")).toBe(null);
+    // ou
+    expect(screen.queryByText("ethereum")).not.toBeInTheDocument();
+
+  });
+});
--- pages/components/CoinsList.tsx
+++ pages/components/CoinsList.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState} from "react";
+
 const CoinsList = () => {
-    return null
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [filterText, setFilterText] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&parkline=false")
+    .then(resp => resp.json())
+    .then(setData)
+  },[]);
+
+  useEffect(() => {
+    if(data) {
+      setFilteredData(data.filter((item : any) => item.id.toLowerCase().includes(filterText.toLowerCase())))
+    }else {
+      setFilteredData([]);
+    }
+  }, [data, filterText])
+ 
+  return ( 
+    <div>
+      <div>
+        <label>
+          Filter
+          <input onChange={(event : any) => setFilterText(event.target.value)} />
+        </label>
+      </div>
+      {filteredData.map((item: any) => (
+        <div>{item.id}</div>
+      ))}
+    </div>
+  );
 };
 
 export default CoinsList;
